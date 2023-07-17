e7523de3376c8bda3b726e6c171d011ef520ad34
周俊杰 <zhoujunjie@Tocmatics-macbookpro-14-2023.local>
2023-07-17 08:18:37 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import mss
+import numpy as np
+
+with mss.mss() as sct:
+    # 设置截取区域，此处为整个屏幕
+    monitor = sct.monitors[0]
+
+    while True:
+        # 截取屏幕图像
+        img = sct.grab(monitor)
+
+        # 将截取的图像数据转换为numpy数组
+        img = np.array(img)
+
+        # 在窗口中显示图像
+        cv2.imshow("Real-time Screenshot", img)
+
+        # 按下 ESC 键退出循环
+        if cv2.waitKey(1) == 27:
+            break
+
+cv2.destroyAllWindows()
